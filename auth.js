916fc390b05163087e5a1f839ac0767739d2ce2c
toonwaeze2000-blip <toonwaeze2000@gmail.com>
2026-09-07 14:21:25 +0200

    Add Supabase authentication client

---
+++ auth.js
@@ -0,0 +1,3 @@
+const SUPABASE_URL = 'https://boaqzdyfsosswqvjgsjn.supabase.co';
+const SUPABASE_KEY = ['sb_publishable_Jlne','B01f5LDJ4VBO_n1E7Q_','jxXn5JBky'].join('');
+window.sixpackAuth = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
